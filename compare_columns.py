2dc12dea8ee74e36ef1b80194fe6557658028d9c
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2021-12-22 13:37:57 -0800

    Update compare_columns.py

--- compare_columns.py
+++ compare_columns.py
@@ -1,6 +1,8 @@
-# Title : Ablebits Compare
+# Title : Compare Columns
 # Author: Jun Kim
-# Date: 11/23/2020
+# Date: 12/22/2021
 # Description: In this program, tkinter will be used to prompt a user to choose a csv file to retrieve data from.
 # The user will then be prompted to choose a second file to compare the email addresses from entries in the first file.
 # If the email addresses match, respective ID will be written in to the new file
+
+
