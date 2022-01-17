5693c50083a67703636efd9abe6ecb602b23b258
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2022-01-17 10:14:17 -0800

    Update and rename compare_columns.py to bounced_emails.py

---
+++ bounced_emails.py
@@ -0,0 +1,7 @@
+# Title : Bounced Emails
+# Author: Jun Kim
+# Date: 12/22/2021
+# Description: This program is a variation of the compare_two_tables program. A tkinter will be used to prompt a user to choose a csv file to retrieve data from.
+# The user will then be prompted to choose a second file to compare the email addresses from entries in the first file.
+# If the email addresses match, respective IDs will be written in to the new file.
+
--- compare_columns.py
+++
@@ -1,8 +0,0 @@
-# Title : Compare Columns
-# Author: Jun Kim
-# Date: 12/22/2021
-# Description: In this program, tkinter will be used to prompt a user to choose a csv file to retrieve data from.
-# The user will then be prompted to choose a second file to compare the email addresses from entries in the first file.
-# If the email addresses match, respective ID will be written in to the new file
-
-
