e54901601b67dcd86565d8de0e43a0889ea359ca
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2022-05-03 10:59:05 -0700

    Update compare_two_tables.py

--- compare_two_tables.py
+++ compare_two_tables.py
@@ -5,22 +5,19 @@
 # One file will contain the primary keys and another column with values for commparison. The second value will contain the values we are comparing with the first CSV file. 
 # The resulting output file will contain only the rows that had a matching value and save to a folder with a timestamp.
 
+
 import pandas as pd                                                        
-import os                                                                   # used to create relative path to write file
-import tkinter as tk                                                        # used as a user friendly tool for the program
+import os                                                                   
+import tkinter as tk                                                       
 from tkinter import *                                                    
 from tkinter.filedialog import askopenfilename
 from datetime import datetime                                             
 
 
-# =======================================================================================================================================================
-#                                                           Script lines for Tkinter GUI
-# =======================================================================================================================================================
+window = tk.Tk()                                                            
+window.geometry('200x200')                                                  
 
-window = tk.Tk()                                                            # creates a tkinter object
-window.geometry('200x200')                                                  # set size of tkinter window
-
-label = tk.Label(text='Compare Two Tables')                                     # sets the text to be dipslayed by tkinter
+label = tk.Label(text='Compare Two Tables')                                     
 label.pack()
 
 def csv_opener():
@@ -28,13 +25,16 @@ def csv_opener():
     global csv_name
     global csv_file
     global csv_df
-
-    csv_name = askopenfilename()                                            # show an "Open" dialog box and return the path to the selected file
-    csv_file = open(csv_name, 'r')		                                    # opens the csv file to read
+    
+    # show an "Open" dialog box and return the path to the selected file
+    csv_name = askopenfilename()                                            
+    csv_file = open(csv_name, 'r')		   
+    
     csv_data = pd.read_csv(csv_file) 
     csv_df = pd.DataFrame(csv_data)  
 
     return None
+
 
 def comparison_opener():
     """ this function is used for the button to open the ids file """
@@ -52,51 +52,58 @@ def comparison_opener():
 
     return None
 
-# =======================================================================================================================================================
-#                                                           Main Function Section
-# =======================================================================================================================================================
+
+# Main Function Section
 
 def main():
-                                                                            # Buttons that will show up on the tkinter window for user
+    
+    # Buttons that will show up on the tkinter window for user
     csv_button = Button(window, text = 'Open CSV File', command = csv_opener).pack() 
     ids_button = Button(window, text = 'Open Comparison File', command = comparison_opener).pack()
 
     window.mainloop()
-
-    dt = datetime.now().strftime('%Y_%m_%d-%I_%M_%S_%p')                    # year_month_day-hours_minutes_seconds_AM/PM ; used in Title                                                            
+    
+    # year_month_day-hours_minutes_seconds_AM/PM ; used in Title    
+    dt = datetime.now().strftime('%Y_%m_%d-%I_%M_%S_%p')                                                                    
 
     input_file = os.path.basename(csv_name)
-    file_name = str(dt) +" Matches from " + input_file                      # sets the file name 
+    file_name = str(dt) +" Matches from " + input_file                      
 
-    script_dir = os.path.dirname(__file__)                                  # absolute directory the script is in
+    script_dir = os.path.dirname(__file__)                                  
     rel_path = 'Output'
-    abs_file_path = os.path.join(script_dir, rel_path)                      # this joins the absolute path of current script with wanted relative path
+    
+    # this joins the absolute path of current script with wanted relative path
+    abs_file_path = os.path.join(script_dir, rel_path)                      
 
     comparison_values = []
     matched_IDs = []
 
     csv_second_column = csv_df.columns[1]
-
-    for i in ids_df['Comparison Values']:                               # The name of the column that contains the comparison values
+    
+     # The name of the column that contains the comparison values
+    for i in ids_df['Comparison Values']:                               
         comparison_values.append(i)
-
-    for i in range(len(csv_df['Key ID'])):                                      # Primary ID 
+    
+    # Primary ID 
+    for i in range(len(csv_df['Key ID'])):                                     
         if csv_df[csv_second_column][i] in comparison_values:
             
             new_data = csv_df['Key ID'][i]
             matched_IDs.append(new_data)
 
     matched_IDs_df = pd.DataFrame(matched_IDs, columns=['Key ID'])
-
     matched_IDs_df[csv_second_column] = ''
-
-    with open(abs_file_path+'/'+file_name, 'w',newline='') as new_file:	    # creates csv to write in
-
-        matched_IDs_df.to_csv(new_file, index=False)                        # writes the dataframe into the new file without the indices
+    
+    
+    with open(abs_file_path+'/'+file_name, 'w',newline='') as new_file:	    
+        
+        # writes the dataframe into the new file without the indices
+        matched_IDs_df.to_csv(new_file, index=False)                        
 
         csv_file.close()
         ids_file.close()
 
+        
 if __name__ == '__main__':
     main()
 
